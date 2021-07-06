6fd697933410cd4c67aa2d835a7e82272daae925
Daniel Carlozzi <Daniel.Carlozzi@disney.com>
2021-07-06 12:46:16 -0300

    working

---
+++ wwwroot/Models/Metric.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace homeMonitor.Models
+{
+
+    public class Metric
+    {
+        public const string HumidityType = "humi";
+        public const string TemperatureType = "temp";
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("_id")]
+        public string Id { get; set; }
+        [BsonElement("timestamp")]
+        public string TimeStamp { get; set; }
+        [BsonElement("type")]
+        public string Type { get; set; }
+        [BsonElement("value")]
+        public double Value { get; set; }
+        [BsonElement("zoneValue")]
+        public double ZoneValue { get; set; }
+    }
+}
+
---
+++ wwwroot/Models/MonitorDatabaseSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace homeMonitor.Models
+{
+    public class MonitorDatabaseSettings : IMonitorDatabaseSettings
+    {
+        public string MetricsCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IMonitorDatabaseSettings
+    {
+        string MetricsCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
